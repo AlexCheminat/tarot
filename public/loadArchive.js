5840250f68a6d0a8f1b3c3a9d99f69498e7b4bb0
z5592322 <z5592322@ad.unsw.edu.au>
2025-05-23 14:31:46 +1000

    Actually linking pages

--- public/loadArchive.js
+++ public/loadArchive.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://lanbxsawcjelsngtawxw.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhbmJ4c2F3Y2plbHNuZ3Rhd3h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MTIzMjYsImV4cCI6MjA2MzM4ODMyNn0.OePJTwjh3sn42LDiHKGpXlLkIFvipHC507KaqOIEy3k";
-const supabase = window.supabase.createClient(YOUR_URL, YOUR_KEY);
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function loadArchive() {
 const { data, error } = await supabase
