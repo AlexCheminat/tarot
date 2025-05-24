349473756d63db10fb8e98bdc08fed2e3aeda163
z5592322 <z5592322@ad.unsw.edu.au>
2025-05-24 10:44:07 +1000

    Actually linking pages

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.OePJTwjh3sn42LDiHKGpXlLkIFvipHC507KaqOIEy3k";
+const SUPABASE_URL = "https://lanbxsawcjelsngtawxw.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhbmJ4c2F3Y2plbHNuZ3Rhd3h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MTIzMjYsImV4cCI6MjA2MzM4ODMyNn0.OePJTwjh3sn42LDiHKGpXlLkIFvipHC507KaqOIEy3k";
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const headers = {
